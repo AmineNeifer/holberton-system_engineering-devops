0ee69fde4ed6f30973c215b161ada52f10ec5807
AmineNeifer <amineneifer2000@gmail.com>
2020-05-04 23:06:00 +0100

    first task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+use RESTapi to return information
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    emp_id = int(argv[1])
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)
+    r = requests.get(url1).json()
+    name = r.get("name")
+    done = 0
+    total = 0
+    all_tasks = []
+    url2 = 'https://jsonplaceholder.typicode.com/todos/'
+    for i in requests.get(url2).json():
+        if i.get('userId') == emp_id:
+            total += 1
+            if i.get('completed'):
+                done += 1
+                all_tasks.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for task in all_tasks:
+        print('\t {}'.format(task))

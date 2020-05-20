ab051a2aa3938e2a8d3a4db5a9e3e40f7d7e894b
AmineNeifer <amineneifer2000@gmail.com>
2020-05-20 23:35:06 +0100

    added a slash for sed to work

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'debugging':
   path     => ['/bin', '/usr/bin', '/usr/sbin'],
   provider => 'shell',
-  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php' /var/www/html/wp-settings.php",
+  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
 }
